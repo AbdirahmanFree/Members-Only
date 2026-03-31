1fd062d28faa9fbe669e4edf6ea2bf250ec924d9
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-03-31 11:59:27 -0400

    set up user sessions

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('node:path');
-const { Pool } = require('pg');
 const express = require('express');
 const expressSession = require('express-session');
 const passport = require('passport');
@@ -10,17 +9,33 @@ require('dotenv').config()
 
 const app = express();
 
-app.use("views", path.join(__dirname,"views"));
-app.use('view engine', 'ejs');
+app.set("views", path.join(__dirname,"views"));
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 app.use(expressSession({
     store: new pgSession({
         pool: pool,
-        tableName: 'user_sessions'
+        tableName: 'user_sessions',
+        createTableIfMissing: true
     }),
     secret: process.env.SECRET_KEY,
     resave: false,
-    saveUninitialized: false,
-    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}
+    saveUninitialized: true,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30, httpOnly: true, secure: true},
 }))
+
+app.get("/", (req,res) => {
+    if(!req.session.viewCount){
+        req.session.viewCount = 1;
+    }
+    else {req.session.viewCount++}
+    console.log(req.session.viewCount)
+    res.render("index")
+})
+
+const PORT= 3000
+
+app.listen(PORT, () =>{
+    console.log(`http://localhost:${PORT}`)
+})
