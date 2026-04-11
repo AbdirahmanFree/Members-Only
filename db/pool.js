a6a3189437ce4ce8cefcfaf5a13002986f1835e3
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-04-11 14:47:17 -0400

    changed url for database

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,7 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const pool = new Pool({
-    database: 'members_only',
-    user: 'abdirahman',
-    host: 'localhost',
-    port: 5432
+    connectionString: process.env.DATABASE_URL
 })
 
 module.exports = pool
